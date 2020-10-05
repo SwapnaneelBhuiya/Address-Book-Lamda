a3163ee8adee8be9363b36871603654017c4f76f
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-10-05 13:18:49 +0530

    [swapnaneel]UC8:Ability to search people across multiple addressbook using stream

--- src/test/java/AddressBookLamda/Dictionary.java
+++ src/test/java/AddressBookLamda/Dictionary.java
@@ -1,6 +1,8 @@
 package AddressBookLamda;
 
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 import java.util.Map.Entry;
 public class Dictionary extends AddressBookMain
 {
@@ -8,30 +10,30 @@ public class Dictionary extends AddressBookMain
 	  
         public void search_by_city(String city)
         {
+
             int con=0;
-            System.out.println("People living in the city "+city+" are");
-  		    for(Entry<String, ArrayList<Contact>> ee:dict.entrySet())
+            System.out.println("People living in the state "+city+" are");
+            //Predicate<String> isState= n -> n.equals(Contact.getState());
+            for(Entry<String, ArrayList<Contact>> ee:dict.entrySet())
             {
-                for(Contact i:ee.getValue())
-                {
-                	if(i.getCity().equals(city))
-                {con++;System.out.println(i.getFirst_name()+" "+i.getLast_name());}
-                }
+				ArrayList<Contact> ar=ee.getValue();
+                Stream<Contact> res=ar.stream().filter(n-> n.getCity().equals(city)).peek(n->System.out.println(n.getFirst_name()+" "+n.getLast_name()));
+                con+=(int) ar.stream().filter(n-> n.getState().equals(city)).count();
             }
-            System.out.println("Number of contact person by city= "+con);
+            System.out.println("Number of contact person by state= "+con);
         }
         public void search_by_state(String state)
         {
 
             int con=0;
             System.out.println("People living in the state "+state+" are");
+            //Predicate<String> isState= n -> n.equals(Contact.getState());
             for(Entry<String, ArrayList<Contact>> ee:dict.entrySet())
             {
-                for(Contact i:ee.getValue())
-                {
-                	if(i.getState().equals(state))
-                {con++;System.out.println(i.getFirst_name()+" "+i.getLast_name());}}
-                }
+				ArrayList<Contact> ar=ee.getValue();
+                Stream<Contact> res=ar.stream().filter(n-> n.getState().equals(state)).peek(n->System.out.println(n.getFirst_name()+" "+n.getLast_name()));
+                con+=(int) ar.stream().filter(n-> n.getState().equals(state)).count();
+            }
             System.out.println("Number of contact person by state= "+con);
         }
         public void contact_details()
